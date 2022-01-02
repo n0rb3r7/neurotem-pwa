29fa36d7ac8ba414f915aca5b4f291afe102e6f2
Norbert <n0rb3r7@outlook.com>
2022-01-02 19:42:54 -0300

    Files upload and first styles.

---
+++ scripts.js
@@ -0,0 +1,8 @@
+function showMenu() {
+  var el = document.getElementById("menu");
+  if (el.style.display != "block") {
+    el.style.display = "block";
+  } else {
+    el.style.display = "none";
+  }
+}
